56a9100406921eac45c177c6ba26463289269e20
Miguel Vargas <miguel.vargas@realpage.com>
2023-05-25 00:27:42 -0500

    fixes application

--- Controllers/CarController.cs
+++ Controllers/CarController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace cars_api.Controllers
 {
-    [Route("api/controller")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class CarController : Controller
     {
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using cars_api.Application.Features.Cars.Commands;
+using cars_api.Application.Features.Cars.Queries;
 using cars_api.ApplicationDBContext;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +13,8 @@ builder.Services.AddDbContext<CarContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection"));
 });
+builder.Services.AddMediatR(typeof(GetCars.Handler).Assembly);
+builder.Services.AddAutoMapper(typeof(GetCars.Handler));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,6 +27,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true) // allow any origin 
+    .AllowCredentials());
 
 app.UseHttpsRedirection();
 
