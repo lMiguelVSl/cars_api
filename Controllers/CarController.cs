fa9b5f4eb7a37b10aa47811e7490b6f7d97b184d
Miguel Vargas <miguel.vargas@realpage.com>
2023-05-24 20:06:19 -0500

    car controller

---
+++ Controllers/CarController.cs
@@ -0,0 +1,31 @@
+﻿using cars_api.Application;
+using cars_api.Application.Features.Cars.Commands;
+using cars_api.Application.Features.Cars.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cars_api.Controllers
+{
+    [Route("api/controller")]
+    [ApiController]
+    public class CarController : Controller
+    {
+        private readonly IMediator _mediator;
+        public CarController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("/getCars")]
+        public async Task<ActionResult<List<CarDTO>>> Get()
+        {
+            return await _mediator.Send(new GetCars.carList());
+        }
+
+        [HttpPost("/createCar")]
+        public async Task<ActionResult<Unit>> Post(CreateCar.Execute data)
+        {
+            return await _mediator.Send(data);
+        }
+    }
+}
