e08069433855da797eac67a1802292c9f4567893
Miguel Vargas <miguel.vargas@realpage.com>
2023-05-24 19:59:30 -0500

    get cars handler

--- Application/Features/Cars/Queries/GetCars.cs
+++ Application/Features/Cars/Queries/GetCars.cs
@@ -1,12 +1,31 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using cars_api.ApplicationDBContext;
+using cars_api.Model;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace cars_api.Application.Features.Cars.Queries
 {
     public class GetCars
     {
-        public class Execute : IRequest
+        public class carList : IRequest<List<CarDTO>> { }
+        public class Handler : IRequestHandler<carList, List<CarDTO>>
         {
+            private readonly CarContext _carContext;
+            private readonly IMapper _mapper;
 
+            public Handler(CarContext carContext, IMapper mapper)
+            {
+                _carContext = carContext;
+                _mapper = mapper;
+            }
+
+            public async Task<List<CarDTO>> Handle(carList request, CancellationToken cancellationToken)
+            {
+                var cars = await _carContext._Car.ToListAsync();
+                var carsDTO = _mapper.Map<List<Car>, List<CarDTO>>(cars);
+                return carsDTO;
+            }
         }
     }
 }
