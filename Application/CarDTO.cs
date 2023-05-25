257b2fe30c294bf2d4311124b1b4ffa2735f1b05
Miguel Vargas <miguel.vargas@realpage.com>
2023-05-24 19:53:41 -0500

    mapping and DTO

---
+++ Application/CarDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace cars_api.Application
+{
+    public class CarDTO
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ Application/Mapper/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using cars_api.Model;
+
+namespace cars_api.Application.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Car, CarDTO>();
+        }
+    }
+}
